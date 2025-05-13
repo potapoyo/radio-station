6f16d6da33a95c60c1d5cd114bbdfd4ea474401d
unipst <duckymomo@outlook.jp>
2025-05-13 17:29:49 +0900

    番組内容を修正

--- infrastructure/.terraform.lock.hcl
+++ infrastructure/.terraform.lock.hcl
@@ -6,6 +6,7 @@ provider "registry.terraform.io/hashicorp/aws" {
   constraints = "~> 5.0"
   hashes = [
     "h1:M3M0xoC7l4cKG+9NZuJLtstxICUr2tNfT3B7en1vZTA=",
+    "h1:WwgMbMOhZblxZTdjHeJf9XB2/hcSHHmpuywLxuTWYw0=",
     "h1:ltxyuBWIy9cq0kIKDJH1jeWJy/y7XJLjS4QrsQK4plA=",
     "zh:0cdb9c2083bf0902442384f7309367791e4640581652dda456f2d6d7abf0de8d",
     "zh:2fe4884cb9642f48a5889f8dff8f5f511418a18537a9dfa77ada3bcdad391e4e",
@@ -30,6 +31,7 @@ provider "registry.terraform.io/hashicorp/random" {
   constraints = "~> 3.6"
   hashes = [
     "h1:I8MBeauYA8J8yheLJ8oSMWqB0kovn16dF/wKZ1QTdkk=",
+    "h1:R5Ucn26riKIEijcsiOMBR3uOAjuOMfI1x7XvH4P6B1w=",
     "h1:t0mRdJzegohRKhfdoQEJnv3JRISSezJRblN0HIe67vo=",
     "zh:03360ed3ecd31e8c5dac9c95fe0858be50f3e9a0d0c654b5e504109c2159287d",
     "zh:1c67ac51254ba2a2bb53a25e8ae7e4d076103483f55f39b426ec55e47d1fe211",
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,15 @@
-variable "service" {
+variable "service"{
   type    = string
   default = "radio_station"
 }
 
-variable "bucket_name" {
+variable "bucket_name"{
   type        = string
   default     = "radio-station-potapoyo"
   description = "radio-station-potapoyo"
 }
 
-variable "channels" {
+variable "channels"{
   type = list(object({
     title          = string
     id             = string
@@ -23,140 +23,175 @@ variable "channels" {
     rec_dayweek    = string
     image          = string
   }))
-  default = [{
-    title          = "オードリーのオールナイトニッポン"
-    id             = "audrey"
+  default = [
+    {
+    title          = "伊集院光 深夜の馬鹿力"
+    id             = "ijuin"
     area           = "JP13"
-    station        = "LFR"
-    exec_dayweek   = "SUN"
-    exec_time_min  = 0
-    exec_time_hour = 4
-    image          = "https://radiko.jp/res/program/DEFAULT_IMAGE/IBS/i03z6mzyq4.jpg"
-    rec_dayweek    = "SUN"
+    station        = "TBS"
+    exec_dayweek   = "TUE"
+    exec_time_min  = 3
+    exec_time_hour = 3
+    image          = "https://www.tbs.co.jp/radio/ijuin/free/images/mailform/mainimg_ijuin.jpg"
+    rec_dayweek    = "TUE"
     rec_time_min   = 0
     rec_time_hour  = 1
-    }, {
-    title          = "霜降り明星のオールナイトニッポン"
-    id             = "shimofuri"
+    },
+    {
+    title          = "キタニタツヤのオールナイトニッポン0"
+    id             = "kitani_ann0"
     area           = "JP13"
     station        = "LFR"
-    exec_dayweek   = "SAT"
-    exec_time_min  = 0
+    exec_dayweek   = "TUE"
+    exec_time_min  = 33
     exec_time_hour = 4
-    image          = "https://i.scdn.co/image/ab6765630000ba8acbe64777258e8652af51a129"
-    rec_dayweek    = "SAT"
+    image          = "https://program-static.cf.radiko.jp/7pnwn5cepb.jpg"
+    rec_dayweek    = "TUE"
     rec_time_min   = 0
-    rec_time_hour  = 1
-    }, {
+    rec_time_hour  = 3
+    },
+    {
     title          = "アルコ＆ピースD.C.GARAGE"
     id             = "dcg"
     area           = "JP13"
     station        = "TBS"
     exec_dayweek   = "WED"
-    exec_time_min  = 0
-    exec_time_hour = 3
+    exec_time_min  = 3
+    exec_time_hour = 1
     image          = "https://i.scdn.co/image/ab6765630000ba8aa55f94fad751e63c8f019834"
     rec_dayweek    = "WED"
     rec_time_min   = 0
     rec_time_hour  = 0
-    }, {
-    title          = "ハライチのターン"
-    id             = "haraichi"
-    area           = "JP13"
-    station        = "TBS"
-    exec_dayweek   = "FRI"
-    exec_time_min  = 0
-    exec_time_hour = 3
-    image          = "https://i.scdn.co/image/ab67656300005f1fd89c4d7d72800b603937135f"
-    rec_dayweek    = "FRI"
-    rec_time_min   = 0
-    rec_time_hour  = 0
-    }, {
-    title          = "有吉弘行のSUNDAY_NIGHT_DREAMER"
-    id             = "snd"
-    area           = "JP9"
-    station        = "RADIOBERRY"
-    exec_dayweek   = "SUN"
-    exec_time_min  = 0
-    exec_time_hour = 23
-    image          = "https://park-cf.gsj.mobi/program/appimages/27400/672.jpg"
-    rec_dayweek    = "SUN"
-    rec_time_min   = 0
-    rec_time_hour  = 20
-    }, {
-    title          = "佐久間宣行のオールナイトニッポン0"
-    id             = "sakumazero"
+    },
+    {
+    title          = "星野源のオールナイトニッポン"
+    id             = "hoshinogen_ann"
     area           = "JP13"
     station        = "LFR"
-    exec_dayweek   = "THU"
-    exec_time_min  = 0
-    exec_time_hour = 6
-    image          = "https://i.scdn.co/image/ab6765630000ba8a01a2246aa686f72f2adeb40e"
-    rec_dayweek    = "THU"
-    rec_time_min   = 0
-    rec_time_hour  = 3
-    }, {
-    title          = "おぎやはぎのメガネびいき"
-    id             = "ogiyahagi"
-    area           = "JP13"
-    station        = "TBS"
-    exec_dayweek   = "FRI"
-    exec_time_min  = 0
-    exec_time_hour = 4
-    image          = "https://radiko.jp/res/program/DEFAULT_IMAGE/TBS/mgqwj57j5a.jpg"
-    rec_dayweek    = "FRI"
+    exec_dayweek   = "WED"
+    exec_time_min  = 3
+    exec_time_hour = 3
+    image          = "https://program-static.cf.radiko.jp/0mdmzcoykd.png"
+    rec_dayweek    = "WED"
     rec_time_min   = 0
     rec_time_hour  = 1
     },
     {
-      title          = "山里亮太の不毛な議論"
-      id             = "yamasato"
-      area           = "JP13"
-      station        = "TBS"
-      exec_dayweek   = "THU"
-      exec_time_min  = 0
-      exec_time_hour = 4
-      image          = "https://tbsradio.g.kuroco-img.app/v=1631433516/files/topics/44299_ext_04_0.jpg?width=1200"
-      rec_dayweek    = "THU"
-      rec_time_min   = 0
-      rec_time_hour  = 1
-      }, {
-      title          = "三四郎のオールナイトニッポン0"
-      id             = "sanshirozero"
-      area           = "JP13"
-      station        = "LFR"
-      exec_dayweek   = "SAT"
-      exec_time_min  = 0
-      exec_time_hour = 6
-      image          = "https://podcast.1242.com/wp-content/uploads/2020/03/thumb_annzero_sanshiro2022-e1649379852168.jpg"
-      rec_dayweek    = "SAT"
-      rec_time_min   = 0
-      rec_time_hour  = 3
+    title          = "あののオールナイトニッポン0"
+    id             = "ano_ann0"
+    area           = "JP13"
+    station        = "LFR"
+    exec_dayweek   = "WED"
+    exec_time_min  = 33
+    exec_time_hour = 4
+    image          = "https://program-static.cf.radiko.jp/hh4uzk41lv.jpg"
+    rec_dayweek    = "WED"
+    rec_time_min   = 0
+    rec_time_hour  = 3
     },
     {
-      title          = "ランジャタイによりますと"
-      id             = "ranjatai"
-      area           = "JP16"
-      station        = "KNB"
-      exec_dayweek   = "SUN"
-      exec_time_min  = 0
-      exec_time_hour = 23
-      image          = "https://pbs.twimg.com/card_img/1879876163176407040/B5c-XxoE?format=jpg&name=900x900"
-      rec_dayweek    = "SAT"
-      rec_time_min   = 0
-      rec_time_hour  = 22
-  },
-      {
-      title          = "伊集院光 深夜の馬鹿力"
-      id             = "ijuin"
-      area           = "JP13"
-      station        = "TBS"
-      exec_dayweek   = "TUE"
-      exec_time_min  = 3
-      exec_time_hour = 3
-      image          = "https://www.tbs.co.jp/radio/ijuin/free/images/mailform/mainimg_ijuin.jpg"
-      rec_dayweek    = "TUE"
-      rec_time_min   = 0
-      rec_time_hour  = 1
-  }]
+    title          = "スタンド・バイ・見取り図"
+    id             = "stm"
+    area           = "JP13"
+    station        = "TBS"
+    exec_dayweek   = "THU"
+    exec_time_min  = 3
+    exec_time_hour = 1
+    image          = "https://is1-ssl.mzstatic.com/image/thumb/Podcasts221/v4/f2/f7/68/f2f76820-9740-2e74-7be1-a4a7dd53c498/mza_4890322809227503553.jpg/300x300bb.webp"
+    rec_dayweek    = "THU"
+    rec_time_min   = 0
+    rec_time_hour  = 0
+    },
+    {
+    title          = "佐久間宣行のオールナイトニッポン0"
+    id             = "sakuma_ann0"
+    area           = "JP13"
+    station        = "LFR"
+    exec_dayweek   = "THU"
+    exec_time_min  = 33
+    exec_time_hour = 4
+    image          = "https://program-static.cf.radiko.jp/r7ngzbdkjs.jpg"
+    rec_dayweek    = "THU"
+    rec_time_min   = 0
+    rec_time_hour  = 3
+    },
+    {
+    title          = "四千頭身 都築拓紀のサクラバシ919"
+    id             = "sakurabashithu"
+    area           = "JP27"
+    station        = "OBC"
+    exec_dayweek   = "FRI"
+    exec_time_min  = 3
+    exec_time_hour = 1
+    image          = "https://program-static.cf.radiko.jp/b4cd3bce-a1e9-41a8-ac3f-aacfa0b5dc7a.jpeg"
+    rec_dayweek    = "THU"
+    rec_time_min   = 0
+    rec_time_hour  = 23
+    },
+    {
+    title          = "ハライチのターン！"
+    id             = "ht"
+    area           = "JP13"
+    station        = "TBS"
+    exec_dayweek   = "FRI"
+    exec_time_min  = 3
+    exec_time_hour = 1
+    image          = "https://program-static.cf.radiko.jp/rggcucft68.jpg"
+    rec_dayweek    = "FRI"
+    rec_time_min   = 0
+    rec_time_hour  = 0
+    },
+    {
+    title          = "マヂカルラブリーのオールナイトニッポン0"
+    id             = "magilove_ann0"
+    area           = "JP13"
+    station        = "LFR"
+    exec_dayweek   = "FRI"
+    exec_time_min  = 33
+    exec_time_hour = 4
+    image          = "https://program-static.cf.radiko.jp/afrncdi8xh.jpg"
+    rec_dayweek    = "FRI"
+    rec_time_min   = 0
+    rec_time_hour  = 3
+    },
+    {
+    title          = "霜降り明星のオールナイトニッポン"
+    id             = "shimofuri_ann"
+    area           = "JP13"
+    station        = "LFR"
+    exec_dayweek   = "SAT"
+    exec_time_min  = 3
+    exec_time_hour = 3
+    image          = "https://program-static.cf.radiko.jp/n5u6loc6dl.jpg"
+    rec_dayweek    = "SAT"
+    rec_time_min   = 0
+    rec_time_hour  = 1
+    },
+    {
+    title          = "三四郎のオールナイトニッポン0"
+    id             = "sanshiro_ann0"
+    area           = "JP13"
+    station        = "LFR"
+    exec_dayweek   = "SAT"
+    exec_time_min  = 3
+    exec_time_hour = 5
+    image          = "https://program-static.cf.radiko.jp/oqvinj01rs.jpg"
+    rec_dayweek    = "SAT"
+    rec_time_min   = 0
+    rec_time_hour  = 3
+    },
+    {
+    title          = "オードリーのオールナイトニッポン"
+    id             = "audrey_ann"
+    area           = "JP13"
+    station        = "LFR"
+    exec_dayweek   = "SUN"
+    exec_time_min  = 3
+    exec_time_hour = 3
+    image          = "https://program-static.cf.radiko.jp/40zg3cgaf8.jpg"
+    rec_dayweek    = "SUN"
+    rec_time_min   = 0
+    rec_time_hour  = 1
+    }
+  ]
 }
